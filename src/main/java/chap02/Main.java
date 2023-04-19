07e2e9a6de9b3ac426b5e9a1c257ec971842d66c
Juyoung <juuyoung9575@gmail.com>
2023-04-19 16:52:01 +0900

    Docs: "추가 주석 및 컨벤션 테스트"
    ApplicationContext 주석 추가

--- src/main/java/chap02/Main.java
+++ src/main/java/chap02/Main.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
     public static void main(String[] args) {
+        // 설정 정보 이용해서 빈 객체를 생성한다. (스프링 컨테이너)
         AnnotationConfigApplicationContext ctx =
                 new AnnotationConfigApplicationContext(AppContext.class);
+        // 빈 객체를 제공한다.
         Greeter g = ctx.getBean("greeter", Greeter.class);
         String msg = g.greet("스프링");
         System.out.println(msg);
